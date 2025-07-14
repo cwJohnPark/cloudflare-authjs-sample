b6a0807fe2a52a71980166b87bb7e1fd87928ec6
cwjohnpark <pierrotcw@gmail.com>
2025-07-14 13:47:29 +0900

    fix: infer user language from header

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { i18n } from "@/lib/i18n/i18n-config";
-
-function getLocale(request: NextRequest): string | undefined {
-  // Check if there is any supported locale in the pathname
-  const pathname = request.nextUrl.pathname;
-  const pathnameIsMissingLocale = i18n.locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  );
-
-  // Redirect if there is no locale
-  if (pathnameIsMissingLocale) {
-    const locale = i18n.defaultLocale;
-
-    // e.g. incoming request is /products
-    // The new URL is now /en-US/products
-    return locale;
-  }
-}
+import {getLocale} from "@/lib/utils/header-utils";
 
 export default function middleware(request: NextRequest) {
   // Check if there is any supported locale in the pathname
@@ -29,8 +13,6 @@ export default function middleware(request: NextRequest) {
   if (pathnameIsMissingLocale) {
     const locale = getLocale(request);
 
-    // e.g. incoming request is /products
-    // The new URL is now /en/products
     return NextResponse.redirect(
       new URL(
         `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
